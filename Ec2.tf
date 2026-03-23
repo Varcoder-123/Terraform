9c1186fd084a1e9277e1295b432c6066e33f4e0b
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-03-23 19:17:31 +0530

    new-change

--- Ec2.tf
+++ Ec2.tf
@@ -3,10 +3,18 @@ provider "aws" {
 }
 
 resource "aws_instance" "name" {
-  ami                    = "ami-02dfbd4ff395f2a1b"
-  instance_type          = "t2.micro"
+  count              = 2
+  ami                = "ami-02dfbd4ff395f2a1b"
+  instance_type      = "t2.micro"
+  key_name           = "Hashicrop"
+  vpc_security_group_ids = ["sg-0c2c1c476b1ffca8a"]   
 
   tags = {
-    Name = "Terraform"
+    Name = "Terraform"    
   }
-}
+
+  root_block_device {   
+    volume_size = 10
+    volume_type = "gp3"
+  }
+}    
