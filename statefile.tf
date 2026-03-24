8812c308b3b770a907c8de36dfd2c119f088c62b
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-03-24 10:21:34 +0530

    modified statefile

--- statefile.tf
+++ statefile.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "aws_s3_bucket" {
-  bucket = "23-03-2026"
+  bucket = "24-03-2026"
 
   tags = {
     Name = "MyBucket"
   }
 
 }
-//In this code you have to manually create the s3 bucket later the statefile will upload in s3, but in order to delete the s3 backend you have to delete it first than the s3 bucket manually 
+//In this code you have to manually create the backed s3 bucket later the statefile will upload in s3, We are creating 2 buckets because we cant create the backend s3 alone as this is not resource. So new s3 bucket will be created along with backend s3 will point to existing bucket
 terraform {
   backend "s3" {
     bucket         = "23-03-2026"
